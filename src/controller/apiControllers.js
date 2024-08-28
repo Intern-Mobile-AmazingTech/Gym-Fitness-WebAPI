e66aa9a4d8bfc9efc8369d2644ff31c40ee80249
vtttruc <vothithanhtruc0307@gmail.com>
2024-08-29 01:34:33 +0700

    back end updatePassword, checkExist

--- src/controller/apiControllers.js
+++ src/controller/apiControllers.js
@@ -28,7 +28,45 @@ const signup = async (req, res) => {
     }
 }
 
+const checkExist = async (req, res) => {
+    try {
+        let {user_email} = req.body;
+        const pool = await connectDB();
+        const result = await pool.request().query(`select * from UserAccount where user_email = '${user_email}'`);
+        if (result.rowsAffected[0] > 0) {
+            console.log(user_email);
+            return res.status(400).json({ message: "Email already exists" });
+        }
+        else {
+            return res.status(200).json({ message: "Email is available" });
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const updatePassword = async (req, res) => {
+    try {
+        let {user_email, user_password} = req.body;
+        const pool = await connectDB();
+        const result = await pool.request().query(`select * from UserAccount where user_email = '${user_email}'`);
+        if (result.rowsAffected[0] < 0) {
+            return res.status(404).json({ message: "Email not found" });
+        }
+
+        const result1 = await pool.request().query(`update UserAccount set user_password = '${user_password}' where user_email = '${user_email}'`);
+        return res.status(200).json({ message: "Success" });
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+
 module.exports = {
     test,
     signup,
+    checkExist,
+    updatePassword
 }
