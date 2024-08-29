170717f0277afe3cd02a0a49ad7e5165d00418ed
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2024-08-29 13:43:49 +0700

    adjust signup

--- src/controller/apiControllers.js
+++ src/controller/apiControllers.js
@@ -8,7 +8,7 @@ let test = async (req, res) => {
 
 const signup = async (req, res) => {
     try {
-        let { user_id, user_email, user_fullname, user_phone } = req.body;
+        let { user_id, user_email, user_fullname, user_phone, isNormalUser } = req.body;
         const pool = await connectDB();
         const result = await pool.request().query(`Select * from UserAccount where user_email = '${user_email}'`);
         if (result.rowsAffected[0] > 0) {
@@ -16,8 +16,14 @@ const signup = async (req, res) => {
         }
         else {
             try {
-                const result = await pool.request().query(`Insert into UserAccount (user_id, user_email, user_fullname, user_phone) values ('${user_id}', '${user_email}', '${user_fullname}', '${user_phone}')`);
-                return res.status(200).json({ message: "Success" });
+                if (isNormalUser != null){
+                    const result = await pool.request().query(`Insert into UserAccount(user_id, user_email, user_fullname, user_phone, isNormalUser) values ('${user_id}', '${user_email}', '${user_fullname}', '${user_phone}', '${isNormalUser}')`);
+                    return res.status(200).json({ message: "Success" });
+                }
+                else {
+                    const result = await pool.request().query(`Insert into UserAccount(user_id, user_email, user_fullname, user_phone) values ('${user_id}', '${user_email}', '${user_fullname}', '${user_phone}')`);
+                    return res.status(200).json({ message: "Success" });
+                }
             }
             catch (err) {
                 console.log(err);
